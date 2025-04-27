6c59abdfb3f6461493b2d85bf39dcac1f01d6134
Soumyajit Banerjee <banerjeesoumyajit2005@gmail.com>
2025-04-28 00:46:09 +0530

    2nd commit

--- firstProgram.kt
+++ firstProgram.kt
@@ -1,3 +1,7 @@
 fun main(){
-    println("Hello from Soumyajit! Excited to start Kotlin");
+    println("Hello from Soumyajit! Excited to start Kotlin")
+    name()
+}
+fun name(){
+    println("Introduction :\nHi im Soumyajit Banerjee \n"+"Im a Spring Boot Developer and im now want to skilled up myself in Kotlin")
 }
