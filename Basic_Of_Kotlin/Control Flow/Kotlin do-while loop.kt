b6e29d2d961741e9722d742f65154920ae27712a
Soumyajit Banerjee <banerjeesoumyajit2005@gmail.com>
2025-05-05 22:53:11 +0530

    Kotlin do-while loop

---
+++ Basic_Of_Kotlin/Control Flow/Kotlin do-while loop.kt
@@ -0,0 +1,16 @@
+package Basic_Of_Kotlin.`Control Flow`
+
+//TODO docs : https://www.geeksforgeeks.org/kotlin-do-while-loop/
+
+//do {
+//    // code to run
+//}
+//while(condition)
+
+fun main(){
+    var num : Int = 1
+    do {
+        println(num)
+        num++
+    }while (num<=10)
+}
